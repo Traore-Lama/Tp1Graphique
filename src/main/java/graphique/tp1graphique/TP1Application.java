a70b8bf4b5b5422548f8707172911e9e59c7952f
Traore-Lama <traorelama997@gmail.com>
2026-01-22 17:42:34 +0100

    Fin 3eme Page

--- src/main/java/graphique/tp1graphique/TP1Application.java
+++ src/main/java/graphique/tp1graphique/TP1Application.java
@@ -12,6 +12,13 @@ public class TP1Application extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TP1Application.class.getResource("tp1-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
+        // Connexion de la feuille de style CSS
+        scene.getStylesheets().add(
+                TP1Application.class
+                        .getResource("style.css")
+                        .toExternalForm()
+        );
         stage.setTitle("TP1 !");
         stage.setScene(scene);
         stage.show();
--- src/main/java/graphique/tp1graphique/TP1Controller.java
+++ src/main/java/graphique/tp1graphique/TP1Controller.java
@@ -1,11 +1,18 @@
 package graphique.tp1graphique;
 
+import javafx.animation.RotateTransition;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.transform.Translate;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -24,6 +31,13 @@ public class TP1Controller implements Initializable {
     private AnchorPane apMenu1;
     @FXML
     private Button btnMenu3;
+    @FXML
+    private Circle cirMoove;
+    @FXML
+    private ImageView imgSettings;
+
+    private TranslateTransition translateTransition;
+    private RotateTransition rotateTransition;
 
     @FXML
     public void btnMenu3Clicked(Event event) {
@@ -40,11 +54,53 @@ public class TP1Controller implements Initializable {
         apMenu1.toFront();
     }
 
-
     // Au chargement du Formulaire
     // Au Lancement de l'Appli
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         apMenu1.toFront();
+
+        translateTransition = new TranslateTransition(Duration.seconds(1), cirMoove);
+        translateTransition.setFromX(10);
+        translateTransition.setToX(400);
+        translateTransition.setAutoReverse(true);
+        translateTransition.setCycleCount(Timeline.INDEFINITE);
+
+
+        rotateTransition = new RotateTransition(Duration.seconds(1), imgSettings);
+        rotateTransition.setByAngle(360);
+        rotateTransition.setCycleCount(Timeline.INDEFINITE);
+
+    }
+
+    // Effet quand on clique sur un boutton
+    @FXML
+    public void btnAjClicked(Event event) {
+
+    }
+
+    @FXML
+    public void btnMoClicked(Event event) {
+    }
+
+    @FXML
+    public void btnSuClicked(Event event) {
+    }
+
+
+    // Lancement de L'event Page 3
+    @FXML
+    public void btnAnimClicked(Event event) {
+    translateTransition.play();
+    rotateTransition.play();
+}
+
+    // Arret de L'event Page 3
+    @FXML
+    public void btnStopClicked(Event event){
+        translateTransition.stop();
+        rotateTransition.stop();
     }
 }
+
+
